9284aed5e8dd968822d481d7ad90731351eb8f99
Fernando Freitas <ylfernando3@gmail.com>
2021-01-06 20:35:53 -0300

    Enviando o primeiro commit com configurações inicias feitas

---
+++ Assets/JS/script.js
@@ -0,0 +1,7 @@
+/*$('.scroll').on('click', function(e) {
+		e.preventDefault();
+		var id = $(this).attr('href'), targetOffset = $(id).offset().top;
+		$('html', 'body').animate( {
+			scrollTop: targetOffset
+		}, 600);
+	});
