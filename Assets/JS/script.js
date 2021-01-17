f4101a126c14dc7c2573cbb15686d5a772756d40
ylfernando <ylfernando1@gmail.com>
2021-01-16 22:23:37 -0300

    Enviando slides inicias como plugin Cycle

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,10 +1,17 @@
+$(document).ready(function(){
+
 $('.btn-menu-mobile').on('click', function() {
 	$(this).toggleClass('active');
-})
+});
 
 $('#menu-mobile-span').on('click', function() {
 	var menu = $('.ul-header');
 	$(menu).toggle('.ul-header');
 	$(menu).css('flex-direction', 'column');
 	
-})
+});
+
+$('.slide-area').cycle({
+
+});
+});
